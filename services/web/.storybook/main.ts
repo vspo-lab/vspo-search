4d0905fc362b107c5e6cb0ff3a574eff76a1c290
sugar-cat7 <study.king7777@gmail.com>
2026-02-17 22:13:32 +0900

    add storybook workflow

--- services/web/.storybook/main.ts
+++ services/web/.storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-essentials",
     "@storybook/addon-a11y",
   ],
   framework: {
